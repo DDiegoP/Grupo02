8bd9eb64846de75c5e30aa3e443e4c788c45feff
DiegoLopez <diegol08@ucm.es>
2022-03-14 11:31:13 +0100

    Archivos iniciales añadidos al índice del repositorio

---
+++ Grupo02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Grupo02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
